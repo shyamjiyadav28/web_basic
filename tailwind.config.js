cea67593c9c1151f8a9fd50a8e2c4f23d605588a
Shyamji Yadav <shyamji.yadav.cs28@iilm.edu>
2026-09-21 10:22:42 +0530

    Add restaurant website

---
+++ tailwind.config.js
@@ -0,0 +1,3 @@
+/** @type {import('tailwindcss').Config} */
+export default { content: ['./index.html', './src/**/*.{js,jsx}'], theme: { extend: { colors: { ink:'#171412', cream:'#f8f3eb', ember:'#c77636', gold:'#e1ac61' }, fontFamily: { display:['Playfair Display','serif'], sans:['DM Sans','sans-serif'] } } }, plugins: [] }
+
